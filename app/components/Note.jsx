77eb53e5b398bdf4c971ebe81ff93d5933fcb124
Clara Izquierdo <cizquierdo@lciberica.es>
2015-12-04 14:59:34 +0100

    Create Note

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import Button from 'react-toolbox/lib/button';
 
 export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+  }
   render() {
     return <div>{this.props.task} <Button label='Bookmark' raised primary /></div>;
   }
